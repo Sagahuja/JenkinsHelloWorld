f6858713a7d74896232c64c86ee7397aeca2234f
Sagar Ahuja <sagar.ahuja0910@gmail.com>
2021-09-20 21:39:08 +0530

    Adding New Files

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+package ProblemStatements_Practice;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is Basically for Jenkins Learning");
+		System.out.println("It is a Hello World Class");
+
+	}
+
+}
